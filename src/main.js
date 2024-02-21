e891b98f1b9528b7098f934f49fd75b7546dbc37
Du_xc <xc@163.com>
2024-02-21 14:16:53 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import api from './api'
 import './assets/style/index.scss'
+import './assets/style/element-theme.scss'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 Vue.prototype.$api = api
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="login">
-    <div class="login-bg"></div>
-    <div class="login-box"></div>
+    <div class="login-box">
+      <div class="login-text">酒之家</div>
+      <div class="login-form-box">
+        <el-form :model="loginForm"
+                 :rules="rules"
+                 class="login-form"
+                 ref="loginForm"
+                 @submit.native.prevent>
+          <el-form-item prop="account"
+                        class="login-form-input">
+            <el-input v-model="loginForm.account"
+                      placeholder="账号/手机号"
+                      clearable></el-input>
+          </el-form-item>
+          <el-form-item prop="password"
+                        class="login-form-input">
+            <el-input type="password"
+                      v-model="loginForm.password"
+                      placeholder="密码"
+                      clearable
+                      @keyup.enter.native="submitForm('loginForm')"></el-input>
+          </el-form-item>
+          <el-button type="primary"
+                     class="login-form-button"
+                     :loading="loading"
+                     @click="submitForm('loginForm')">{{ loading ? '登录中' : '登录' }}</el-button>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,38 +37,109 @@ export default {
   name: 'login',
   data () {
     return {
-
+      loading: false,
+      loginForm: {
+        account: '',
+        password: ''
+      },
+      rules: {
+        account: [
+          { required: true, message: '请输入账号/手机号', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
     }
-  }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.loading = true
+          this.$message({
+            message: '登录成功',
+            type: 'success'
+          })
+          setTimeout(() => {
+            this.loading = false
+          }, 2000)
+        } else {
+          return false
+        }
+      })
+    }
+  },
 }
 </script>
 
 <style lang="scss">
 .login {
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
-  justify-content: flex-end;
-
-  .login-bg {
-    width: 100%;
-    height: 100%;
-    background: url('../../assets/img/login_bg.jpeg') no-repeat;
-    background-size: 100% 100%;
-    position: absolute;
-  }
+  justify-content: center;
+  align-items: center;
+  background: url('../../assets/img/login-bg.jpg') no-repeat;
+  background-size: 100% 100%;
 
   .login-box {
-    width: 300px;
-    height: 500px;
-    position: relative;
-    z-index: 2;
-    margin: 120px 100px 0 0;
+    width: 400px;
+    height: 320px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
     box-shadow: 0px 2px 40px rgba(0, 0, 0, 0.1);
     padding: 40px;
     background-color: #fff;
     border-radius: 4px;
     opacity: .9;
+
+    .login-text {
+      font-size: 30px;
+      font-weight: 700;
+      color: #409eff;
+      margin-bottom: 20px;
+    }
+
+    .login-form-box {
+      width: 100%;
+      height: 100%;
+
+      .login-form {
+        padding: 24px;
+
+        .login-form-input {
+          width: 100%;
+          margin-bottom: 25px;
+
+          .el-form-item__content {
+            line-height: 38px;
+
+            .el-input__inner {
+              font-size: 16px;
+              padding-left: 15px;
+              width: 100%;
+              height: 38px;
+              line-height: 38px;
+            }
+          }
+        }
+
+        .login-form-button {
+          width: 100%;
+          color: #fff;
+          font-size: 14px;
+          background-color: #409eff;
+          border: none;
+          padding: 0;
+          margin-top: 30px;
+          height: 38px;
+          line-height: 38px;
+        }
+      }
+    }
   }
 }
 </style>
